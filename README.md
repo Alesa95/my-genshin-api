2809854e50479334a60058ecab6daab9fe647b6f
Alejandra <alejandra_santiago_95@hotmail.com>
2025-02-03 18:35:53 +0100

    primer endpoint testeado

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-"<h1>My Genshin API</h1>" 
+<h1>My Genshin API</h1>
+<h2>Endpoints</h2>
+<h3>Characters</h3>
+<h3>Regions</h3>
--- app.js
+++ app.js
@@ -1,9 +1,37 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const fs = require('fs');
+const path = require('path');
 
 app.get("/", function (req, res) {
   res.send("¡Hola mundo!");
+});
+
+app.get('/characters', (req, res) => {
+    const filePath = path.join(__dirname, '/data/characters.json');
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Error al leer el archivo JSON');
+        }
+  
+        // Parsea el JSON y lo envía como respuesta
+        const jsonData = JSON.parse(data);
+        res.json(jsonData);
+    });
+});
+
+app.get('/characters/:id', (req, res) => {
+    const filePath = path.join(__dirname, '/data/characters.json');
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Error al leer el archivo JSON');
+        }
+  
+        // Parsea el JSON y lo envía como respuesta
+        const jsonData = JSON.parse(data);
+        res.json(jsonData);
+    });
 });
 
 app.listen(port, function () {
